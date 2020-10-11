4fe56e73380240cbe253bb13bc5f3aaef19c3493
Richie <richie.guaman@gmail.com>
2020-10-11 16:47:27 -0400

    update

---
+++ Javahw_1008.java
@@ -0,0 +1,30 @@
+/**
+ * Javahw_1008
+ */
+
+
+//  Hw for problem 1
+//  k=80 
+//  i=0
+//  k=k-12
+//  k=68
+//  i=1
+//  k=k-12
+//  k=56
+//  i=2
+//  k=k-12
+//  k=44
+//  i=3
+//  k=k-12
+//  k=32
+//  i=4
+//  k=k-12
+//  k=20
+//  i=5
+//  function does not run therefore the solution is k=20 which corresponds to C)
+public class Javahw_1008 {
+
+    public static void main(String[] args) {
+        
+    }
+}
