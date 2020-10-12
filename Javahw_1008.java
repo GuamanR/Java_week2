3444cee8ddae67e06cb1ef1f39b166b44cf018f5
Richie <richie.guaman@gmail.com>
2020-10-12 08:55:05 -0400

    updated file

--- Javahw_1008.java
+++ Javahw_1008.java
@@ -1,3 +1,5 @@
+import sun.tools.jstat.SymbolResolutionClosure;
+
 /**
  * Javahw_1008
  */
@@ -22,9 +24,22 @@
 //  k=20
 //  i=5
 //  function does not run therefore the solution is k=20 which corresponds to C)
+import java.util.Scanner;
 public class Javahw_1008 {
 
+
+
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many lines of code would you like?");
+        int timez = input.nextInt();
+        for (int i = 0; i <=timez; i++){
+            char thang = '*';
+            for (int j = 0; j<i; j++){
+                System.out.printf("%s", thang);
+            }
+        System.out.println("\n");   
+        }
         
     }
 }
